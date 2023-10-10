53a2582d97ce93cea238428df0f7c90a16a90b79
Valeriia Pavlova <happyamanita@gmail.com>
2023-10-10 23:02:36 +0200

    created navbar (unfortunately not pixel-perfect)

--- src/components/exercise/exerciseTailwindOneExercise.tsx
+++ src/components/exercise/exerciseTailwindOneExercise.tsx
@@ -1,13 +1,13 @@
 const steps = [
   { id: "01", name: "Job details", status: "complete" },
   { id: "02", name: "Application form", status: "current" },
-  { id: "03", name: "Preview", status: "upcoming" }
+  { id: "03", name: "Preview", status: "upcoming" },
 ] as const;
 
 const ExerciseTailwindOneExercise = () => {
   return (
     <nav>
-      <ul className="">
+      <ul className="grid grid-cols-3 divide-x border justify-evenly rounded items-center">
         {steps.map((step, stepIdx) => (
           <Step key={step.id} step={step} stepIdx={stepIdx} />
         ))}
@@ -27,10 +27,10 @@ interface StepProps {
 
 const Step = ({ step, stepIdx }: StepProps) => {
   return (
-    <li key={step.name} className="">
+    <li key={step.name} className="flex justify-start p-2">
       {step.status === "complete" ? (
-        <span className="">
-          <span className="">
+        <span className="flex flex-row gap-2 items-center">
+          <span className="rounded-full bg-indigo-600 p-1 text-xs">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -46,22 +46,26 @@ const Step = ({ step, stepIdx }: StepProps) => {
               />
             </svg>
           </span>
-          <span className="">{step.name}</span>
+          <span className="text-xs">{step.name}</span>
         </span>
       ) : step.status === "current" ? (
         <div className="">
           <span className="">
-            <span className="">{step.id}</span>
+            <span className="text-xs rounded-full border-2 m-3 p-2 border-indigo-600 text-indigo-600">
+              {step.id}
+            </span>
           </span>
-          <span className="">{step.name}</span>
+          <span className="text-xs text-indigo-600">{step.name}</span>
         </div>
       ) : (
         <div className="">
           <span className="">
             <span className="">
-              <span className="">{step.id}</span>
+              <span className="rounded-full border-2 m-3 p-2 border-slate-300 text-slate-500 text-xs">
+                {step.id}
+              </span>
             </span>
-            <span className="">{step.name}</span>
+            <span className="text-slate-500 text-xs">{step.name}</span>
           </span>
         </div>
       )}
